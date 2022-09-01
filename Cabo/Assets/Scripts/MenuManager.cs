e62708d0c4299b1856a0bf0c537862e801a54f62
Aryaman Gupta <aryamangupta2002@gmail.com>
2022-09-01 12:19:45 +0530

    level design: add settings menu with the option to change regions
    networking: allow for changing of region and checking of ping
    level design: add indicators for region and ping in menus

--- Cabo/Assets/Scripts/MenuManager.cs
+++ Cabo/Assets/Scripts/MenuManager.cs
@@ -28,16 +28,43 @@ public class MenuManager : MonoBehaviourPunCallbacks
     public TMP_Text error_text;
     public GameObject lobby; 
     public Button startButton;
+    public GameObject regions;
+    public string newRegion;
+    public TMP_Text regionHint_text;
+    public TMP_Text regionPing_text;
+    public TMP_Text roomRegionPing_text;
 
 
     public bool firstLogin = true;
 
     void Start()
     {
+        ServerSettings.ResetBestRegionCodeInPreferences();	
         PhotonNetwork.ConnectUsingSettings();
         GameManager.Instance.localSetGameState(GameState.NONE);
         closeAll();
         openMenu("loading");
+    }
+
+    string getCurrentRegion()
+    {
+        //each region can be uniquely identifued based on the first 2 chars
+        switch(PhotonNetwork.CloudRegion[0..2])
+        {
+            case "as":
+                return "Singapore:";
+            case "au":
+                return "Australia:";
+            case "us":
+                return "USA:";
+            case "eu":
+                return "Netherlands:";
+            case "ca":
+                return "Canada:";
+            case "in":
+                return "India:";
+        }
+        return "";
     }
 
     //Menu button functions
@@ -56,6 +83,16 @@ public class MenuManager : MonoBehaviourPunCallbacks
         openMenu("roomname");
     }
 
+    public void Button_settingsClicked()
+    {
+        openMenu("settings");
+        string currRegion = PhotonNetwork.CloudRegion;
+        Debug.Log("THE FIXED REGION IS " + PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion);
+        //Fixed region adds two extra chars to the CloudRegion, so remove those
+        if(currRegion.Length > 3){ currRegion = PhotonNetwork.CloudRegion[0..^2]; }
+        regions.transform.Find(currRegion).GetComponent<Toggle>().isOn = true;
+    }
+
     public void Button_createRoomClicked()
     {
         if(string.IsNullOrEmpty(roomname_input.text))
@@ -70,14 +107,23 @@ public class MenuManager : MonoBehaviourPunCallbacks
         newRoom.name = roomname_input.text;
         roomList.Add(newRoom);
     }
+
     public void Button_joinRoomClicked()
     {  
         openMenu("roomlist");
+        regionHint_text.text = "Available rooms in the region " + getCurrentRegion();
     }
 
     public void Button_backFromJoinOrCreate()
     {
-        openMenu("join-select");
+        //change region once settings menu is exited
+        if(newRegion != null)
+        {
+            PhotonNetwork.Disconnect();
+            PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion = newRegion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        else { openMenu("join-select"); }
     }
 
     public void Button_leaveRoom()
@@ -93,22 +139,38 @@ public class MenuManager : MonoBehaviourPunCallbacks
         openMenu("loading");
     }
 
-    public void Button_startGameClickec()
+    public void Button_startGameClicked()
     {
-        if (PhotonNetwork.IsMasterClient)
+        if (PhotonNetwork.IsMasterClient) { PhotonNetwork.LoadLevel("Play Level"); }
+    }
+
+    public void setRegion()
+    {
+        Toggle[] toggles = regions.GetComponentsInChildren<Toggle>();
+        foreach (Toggle child in toggles)
         {
-            PhotonNetwork.LoadLevel("Play Level");
+            if(child.isOn) 
+            { 
+                child.GetComponent<Image>().color = Color.green; 
+                if(!PhotonNetwork.CloudRegion.Contains(child.name)) { newRegion = child.name; }
+            }
+            else { child.GetComponent<Image>().color = Color.white; }
         }
     }
 
     //Photon functions
     public override void OnConnectedToMaster()
     {
-        Debug.Log("Connected to master");        
+        Debug.Log("Connected to master in region " + PhotonNetwork.CloudRegion + " with ping " + PhotonNetwork.GetPing());        
         PhotonNetwork.AutomaticallySyncScene = true;
         PhotonNetwork.JoinLobby();
         openMenu("loading");
-    }
+        //reset region and set ping indicators
+        newRegion = null;
+        string pingText = getCurrentRegion() + "\n" + PhotonNetwork.GetPing() + " ms";
+        regionPing_text.text = pingText;
+        roomRegionPing_text.text = pingText;
+    } 
 
     public override void OnJoinedLobby()
     {
@@ -218,8 +280,12 @@ public class MenuManager : MonoBehaviourPunCallbacks
 
     public override void OnDisconnected(DisconnectCause cause)
     {
-        error_text.text = "Lost connection to servers. Please restart the game";
-        openMenu("error");
+        if(newRegion != null ) { openMenu("loading"); }
+        else 
+        { 
+            error_text.text = "Lost connection to servers. Please restart the game"; 
+            openMenu("error");
+        }
     }
 
 
